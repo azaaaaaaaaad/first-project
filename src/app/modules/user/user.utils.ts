5617e81044c46d5deef69a755a2fa194da869c02
azaaaaaaaaad <jrazad10@gmail.com>
2025-01-27 12:13:31 +0600

    feat: enhance generatedStudentId utility to maintain semester and year consistency in ID generation

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -21,7 +21,22 @@ const findLastStudentId = async () => {
 
 //generatedId
 const generatedStudentId = async (payload: TAcademicSemester) => {
-  const currentId = (await findLastStudentId()) || (0).toString();
+  let currentId = (0).toString();
+
+  const lastStudentId = await findLastStudentId();
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+  const lastStudentYear = lastStudentId?.substring(0, 4);
+  const currentSemesterCode = payload.code;
+  const currentYearCode = payload.year;
+
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentYear === currentYearCode
+  ) {
+    currentId = lastStudentId.substring(6);
+  }
+
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
 
   incrementId = `${payload.year}${payload.code}${incrementId}`;
