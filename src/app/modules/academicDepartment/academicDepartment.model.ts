5ab26e79ce03cd6b3a055f784191a6f9cbc79461
azaaaaaaaaad <jrazad10@gmail.com>
2025-01-27 15:56:32 +0600

    feat: enhance academic department model with existence checks and validation on save and update

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -1,19 +1,42 @@
 import { model, Schema } from 'mongoose';
 import { TAcademicDepartment } from './academicDepartment.interface';
 
-const academicDepartmentSchema = new Schema<TAcademicDepartment>({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
+const academicDepartmentSchema = new Schema<TAcademicDepartment>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    academicFaculty: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicFaculty',
+    },
   },
-  academicFaculty: {
-    type: Schema.Types.ObjectId,
-    ref: 'AcademicFaculty'
+  {
+    timestamps: true,
+  },
+);
+
+academicDepartmentSchema.pre('save', async function (next) {
+  const isDepartmentExist = await AcademicDepartmentModel.findOne({
+    name: this.name,
+  });
+
+  if (isDepartmentExist) {
+    throw new Error('This Department Alredy Exist');
   }
-},
-{
-    timestamps: true
+  next();
+});
+
+academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery();
+  const isDepartmentExist = await AcademicDepartmentModel.findOne(query);
+
+  if (!isDepartmentExist) {
+    throw new Error('This department doesnt exist');
+  }
+  next();
 });
 
 export const AcademicDepartmentModel = model<TAcademicDepartment>(
