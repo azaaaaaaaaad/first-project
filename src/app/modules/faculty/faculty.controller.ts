51896d31c19536bee3cf5bb42293cef04572fc1e
azaaaaaaaaad <jrazad10@gmail.com>
2025-01-28 18:16:10 +0600

    feat: add update and delete functionality to faculty controller

--- src/app/modules/faculty/faculty.controller.ts
+++ src/app/modules/faculty/faculty.controller.ts
@@ -26,3 +26,36 @@ const getSingleFaculty = catchAsync(async (req, res) => {
       data: result,
     });
   });
+
+  const updateFaculty = catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const { faculty } = req.body;
+    const result = await FacultyServices.updateFacultyIntoDB( id , faculty);
+  
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Faculty is updated succesfully',
+      data: result,
+    });
+  });
+  
+
+  const deleteFaculty = catchAsync(async (req, res) => {
+    const {  id } = req.params;
+    const result = await FacultyServices.deleteFacultyFromDB(id);
+  
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Faculty is deleted succesfully',
+      data: result,
+    });
+  });
+  
+  export const FacultyControllers = {
+    getAllFaculties,
+    getSingleFaculty,
+    deleteFaculty,
+    updateFaculty,
+  };
