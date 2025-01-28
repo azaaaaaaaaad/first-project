fde07253ea50e08b8d5dfeb1f9323c387ffea47b
azaaaaaaaaad <jrazad10@gmail.com>
2025-01-28 12:47:47 +0600

    feat: enhance global error handling with Zod and Mongoose validation, add NODE_ENV configuration

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -7,5 +7,6 @@ export default {
     port: process.env.PORT || 5000,
     database_url: process.env.DATABASE_URL || '',
     bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS || 12,
-    default_password: process.env.DEFAULT_PASSOWRD
+    default_password: process.env.DEFAULT_PASSOWRD,
+    node_env: process.env.NODE_ENV
 }
--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,23 +1,46 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { Request, Response, NextFunction } from 'express';
- const globalErrorHandler = (
-  error: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const statusCode = error.statusCode || 500;
-  const message = error.message || 'something went wrong';
+import { ErrorRequestHandler } from 'express';
+import { ZodError, ZodIssue } from 'zod';
+import config from '../config';
+import { handleZodError } from '../errors/handleZodError';
+import { handleValidationError } from '../errors/handleValidationError';
+
+const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
+  let statusCode = error.statusCode || 500;
+  let message = error.message || 'something went wrong';
+
+  type TErrorSources = {
+    path: string | number;
+    message: string;
+  }[];
+
+  let errorSources: TErrorSources = [
+    {
+      path: '',
+      message: 'something went wrong',
+    },
+  ];
+
+  if (error instanceof ZodError) {
+    const simplifiedError = handleZodError(error);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (error?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSources = simplifiedError.errorSources;
+  }
 
   return res.status(statusCode).json({
     success: false,
     message,
-    error: error,
+    errorSources,
+    stack: config.node_env === 'development' ? error?.stack : null,
   });
 };
-
 
 export default globalErrorHandler;
