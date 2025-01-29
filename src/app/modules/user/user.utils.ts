ce7b7c6fb3001123cb949b1988513ea97782ad34
azaaaaaaaaad <jrazad10@gmail.com>
2025-01-29 10:46:50 +0600

    feat: add createFacultyIntoDB function and ID generation for faculty management

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -1,7 +1,7 @@
 // year semesterCode 4digit number
 
-import { TAcademicSemester } from "../academicSemester/academicSemester.interface";
-import { UserModel } from "./user.model";
+import { TAcademicSemester } from '../academicSemester/academicSemester.interface';
+import { UserModel } from './user.model';
 
 const findLastStudentId = async () => {
   const lastStudent = await UserModel.findOne(
@@ -48,3 +48,66 @@ export const generateStudentId = async (payload: TAcademicSemester) => {
 
   return incrementId;
 };
+
+//faculty
+export const findLastFacultyID = async () => {
+  const lastFaculty = await UserModel.findOne(
+    {
+      role: 'faculty',
+    },
+    {
+      id: 1,
+      _id: 0,
+    },
+  )
+    .sort({
+      createdAt: -1,
+    })
+    .lean();
+
+  return lastFaculty?.id ? lastFaculty?.id?.substring(2) : undefined;
+};
+
+export const generateFacultyId = async () => {
+  let currentId = (0).toString();
+  const lastFacultyId = await findLastFacultyID();
+
+  if (lastFacultyId) {
+    currentId = lastFacultyId.substring(2);
+  }
+
+  let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
+  incrementId = `F-${incrementId}`;
+  return incrementId;
+};
+
+//admin
+export const findLastAdminId = async () => {
+  const lastAdmin = await UserModel.findOne(
+    {
+      role: 'admin',
+    },
+    {
+      id: 1,
+      _id: 0,
+    },
+  )
+    .sort({
+      createdAt: -1,
+    })
+    .lean();
+
+  return lastAdmin?.id ? lastAdmin?.id?.substring(2) : undefined;
+};
+
+export const generatedAdminId =async() => {
+  let currentId = (0).toString()
+  const lastAdminId = await findLastAdminId()
+  if (lastAdminId) {
+    currentId = lastAdminId.substring(2)
+  }
+
+  let incrementId = (Number(currentId)+1).toString().padStart(4,'0')
+  incrementId = `A-${incrementId}`
+  return incrementId
+}
