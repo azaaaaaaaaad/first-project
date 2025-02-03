5a1b46257a662b598eb8d420390184cfbf8aa501
azaaaaaaaaad <jrazad10@gmail.com>
2025-02-03 11:54:00 +0600

    feat: enhance authentication middleware to validate user existence and handle token expiration after password changes

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -5,33 +5,61 @@ import httpStatus from 'http-status';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
 import { TUserRole } from '../modules/user/user.interface';
+import { User } from '../modules/user/user.model';
 
-const auth = (...requiredRoles : TUserRole[]) => {
+const  isJWTIssuedBeforePasswordChanged = (passwordChangedTimestamp: Date, jwtIssuedTimeStamp: number) : boolean =>{
+  const passwordChangedTime = new Date (passwordChangedTimestamp).getTime()/1000
+  return passwordChangedTime > jwtIssuedTimeStamp;
+}
+
+
+
+const auth = (...requiredRoles: TUserRole[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization;
     if (!token) {
       throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized');
     }
 
-    //check if the token is valid
-    jwt.verify(
+    const decoded = jwt.verify(
       token,
       config.jwt_access_token as string,
-      function (error, decoded) {
-        if (error) {
-          throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized');
+    ) as JwtPayload;
+
+    const { userId, role, iat } = decoded;
+
+    const isUserExist = await User.findOne({id:userId}).select('+password');
+    if (!isUserExist) {
+      throw new AppError(httpStatus.NOT_FOUND, 'user not found');
+    }
+
+    const isDeleted = isUserExist?.isDeleted;
+    if (isDeleted) {
+      throw new AppError(httpStatus.FORBIDDEN, 'user is deleted');
+    }
+
+    const userStatus = isUserExist?.status;
+    if (userStatus === 'blocked') {
+      throw new AppError(httpStatus.FORBIDDEN, 'user is blocked');
+    }
+
+
+       // Validate if JWT is still valid after password change
+       if (isUserExist.passwordChangedAt && iat) {
+        const isTokenExpired = isJWTIssuedBeforePasswordChanged(isUserExist.passwordChangedAt, iat);
+        if (isTokenExpired) {
+          throw new AppError(httpStatus.UNAUTHORIZED, 'Token expired due to password change');
         }
+      }
 
-        const role = (decoded as JwtPayload).role
+   
 
-        if(requiredRoles && !requiredRoles.includes(role)){
-          throw new AppError(httpStatus.UNAUTHORIZED, `you are not  authorized`)
-        }
+    if (requiredRoles && !requiredRoles.includes(role)) {
+      throw new AppError(httpStatus.UNAUTHORIZED, `you are not  authorized`);
+    }
 
-        req.user = decoded as JwtPayload;
-        next();
-      },
-    );
+    req.user = decoded as JwtPayload;
+    next();
   });
 };
 
