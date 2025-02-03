0a1339ab54662bc6c231eff75445d4f5eeeb1b8f
azaaaaaaaaad <jrazad10@gmail.com>
2025-02-03 10:39:10 +0600

    feat: update change password functionality to include user existence checks and password validation

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -14,9 +14,8 @@ const loginUser = catchAsync(async (req, res) => {
 });
 
 const changePassword = catchAsync(async (req, res) => {
-  const user = req.user;
   const { ...passwordData } = req.body;
-  const result = await AuthServices.changePassword(user, passwordData);
+  const result = await AuthServices.changePassword(req.user, passwordData);
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -3,7 +3,7 @@ import { User } from '../user/user.model';
 import { TLoginUser } from './auth.interface';
 import httpStatus from 'http-status';
 import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../../config';
 
 const loginUser = async (payload: TLoginUser) => {
@@ -53,13 +53,54 @@ const loginUser = async (payload: TLoginUser) => {
 };
 
 const changePassword = async (
-  user: { userId: string; role: string },
-  payload,
+  user: JwtPayload,
+  payload: { oldPassword: string; newPassword: string },
 ) => {
-  const result = await User.findOneAndUpdate({
-    id: user?.userId,
-    role: user?.role,
-  });
+
+  
+  const isUserExist = await User.findOne({ id: user?.userId }).select('+password');
+  if (!isUserExist) {
+    throw new AppError(httpStatus.NOT_FOUND, 'user not found');
+  }
+
+  const isDeleted = isUserExist?.isDeleted;
+  if (isDeleted) {
+    throw new AppError(httpStatus.FORBIDDEN, 'user is deleted');
+  }
+
+  const userStatus = isUserExist?.status;
+  if (userStatus === 'blocked') {
+    throw new AppError(httpStatus.FORBIDDEN, 'user is blocked');
+  }
+
+  const isPasswordCorrect = await bcrypt.compare(
+    payload?.oldPassword,
+    isUserExist?.password,
+  );
+
+  if (!isPasswordCorrect) {
+    throw new AppError(httpStatus.BAD_REQUEST, 'password did not matched');
+  }
+
+  //hash new password
+  const newHashedPasssword = await bcrypt.hash(
+    payload?.newPassword,
+    Number(config.jwt_access_token),
+  );
+
+await User.findOneAndUpdate(
+    {
+      id: user?.userId,
+      role: user?.role,
+    },
+    {
+      password: newHashedPasssword,
+      needsPasswordChange: false,
+      passwordChangedAt: new Date
+    },
+  );
+
+  return null;
 };
 
 export const AuthServices = {
