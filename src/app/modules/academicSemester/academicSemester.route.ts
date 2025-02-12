fb9f45f993d24afc498654b86ce21d006ef68048
azad-xyz <jrazad10@gmail.com>
2025-02-12 23:54:08 +0600

    feat: add authentication middleware for academic semester routes; restrict access to super admin, admin, faculty, and student roles

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import { AcademicSemesterControllers } from './academicSemester.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import { AcademicSemesterValidations } from './academicSemester.validation';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
 
 const router = express.Router();
 
 router.post(
   '/create-academic-semester',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(
     AcademicSemesterValidations.createAcademicSemesterValidationSchema,
   ),
@@ -16,10 +19,13 @@ router.post(
 router.get('/', AcademicSemesterControllers.getAllAcademicSemester);
 router.get(
   '/:semesterId',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin, USER_ROLE.faculty, USER_ROLE.student),
   AcademicSemesterControllers.getSingleAcademicSemester,
 );
 router.patch(
   '/:semesterId',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+
   validateRequest(
     AcademicSemesterValidations.updateAcademicSemesterValidationSchema,
   ),
